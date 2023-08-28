ea6c7396d01dda32a7526b4c15363f1bdf068220
samju200 <samju7778@gmail.com>
2023-08-28 19:02:48 +0100

    un finish busineshhhs

--- hooks/useSpotify.js
+++ hooks/useSpotify.js
@@ -1,6 +1,6 @@
-import { signIn, useSession } from 'next-auth/react';
-import { useEffect } from 'react';
-import SpotifyWebApi from 'spotify-web-api-node';
+import { signIn, useSession } from "next-auth/react";
+import { useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-node";
 
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
@@ -11,8 +11,8 @@ function useSpotify() {
   const { data: session, status } = useSession();
   useEffect(() => {
     if (session) {
-      // if refresh access token attempt falls, direct user to login..
-      if (session.error === 'RefreshAccessTokenError') {
+      // if refresh access token attempt falls, direct user to login......
+      if (session.error === "RefreshAccessTokenError") {
         signIn();
       }
       spotifyApi.setAccessToken(session.user.accessToken);
